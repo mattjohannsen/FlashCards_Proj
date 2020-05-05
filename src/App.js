b535ff7130a2f93f0631f31099d7420551a7988a
mattjohannsen <majohann@hotmail.com>
2020-05-05 17:00:04 -0500

    Created drop down list of stacks to select from using map method from the API Array

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import React, {useState} from 'react';
-import axios from 'axios';
-import './App.css';
-
+import React, {useState, useEffect} from 'react';
 import StackList from './Components/StackList';
 import StackInput from './Components/StackInput';
 import FlashcardList from './Components/FlashcardList';
+import './App.css';
+import axios from 'axios';
 
 function App() {
   const [flashcards, setFlashcards] = useState(MattsTestCards);
+  const currentStack = 1;
+
+  useEffect(() => {
+    axios
+      .get('https://localhost:44393/api/collection')
+      .then(res => {
+
+      console.log(res.data)
+      })
+  }, [])
+
   return (
     <div className=" table table-danger">
       <header className="App-header">
@@ -15,29 +25,15 @@ function App() {
         Flash Card Library
         </h1>
       </header>
-      <table className="center">
-        <thead>
-          <tr>
-            <th>Select a stack of flash cards to begin:</th>
-          </tr>
-          <tr>
-            <th>Current Stack: ||</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>
+      <div>
               <StackInput/>
-              <StackList/>
-            </td>
-          </tr>
-          <tr>
-            <td>
+      </div>
+      <div>
+              <StackList />
+      </div>
+      <div>
               <FlashcardList flashcards={flashcards} />
-            </td>
-          </tr>
-        </tbody>
-      </table>
+      </div>
     </div>
   );
 }
@@ -58,6 +54,62 @@ const MattsTestCards = [
     "definition":"Reusable building blocks for UI using JSX"
   }
 ]
+
+// const MattsTestCards = [
+// 	{
+// 		"id":1,
+// 		"title":"React",
+// 		"cards":[
+// 			{
+// 				"id":1,
+// 				"word":"state",
+// 				"definition":"JS object that holds values for a component"
+// 			},
+// 			{
+// 				"id":2,
+// 				"word":"props",
+// 				"definition":"A way to pass data into components on initialization"
+// 			},
+// 			{
+// 				"id":3,
+// 				"word":"component",
+// 				"definition":"Reusable building blocks for UI using JSX"
+// 			}
+// 			]
+// 	},
+// 	{
+// 		"id":2,
+// 		"title":"C#",
+// 		"cards":[
+// 			{
+// 				"id":4,
+// 				"word":"variable",
+// 				"definition":"Named space in memory"
+// 			},
+// 			{
+// 				"id":5,
+// 				"word":"class",
+// 				"definition":"Template for an object that consists of member variables, constructor, methods"
+// 			},
+// 			{
+// 				"id":6,
+// 				"word":"object",
+// 				"definition":"Instance of a class"
+// 			}
+// 			]
+// 	},
+// 	{
+// 		"id":3,
+// 		"title":"Flutter",
+// 		"cards":[
+// 			{
+// 				"id":7,
+// 				"word":"widget",
+// 				"definition":"Reusable component in Flutter"
+// 			}
+// 			]
+// 	}
+// ]
 
 
 export default App;
--- src/Components/StackList.js
+++ src/Components/StackList.js
@@ -2,9 +2,28 @@ import React from 'react';
 import axios from 'axios';
 
 export default class StackList extends React.Component {
-    state =     {
-        stacks: []
+    constructor(props) {
+        super(props);
+        this.state ={
+            stacks: [],
+            currentStack: 0
+        }
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleChange = this.handleSubmit.bind(this);
     }
+
+
+    handleChange(event) {
+        this.setState({currentStack: event.target.value});
+        console.log(event.target.value);
+      }
+    
+    handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.value);
+    event.preventDefault();
+    }
+
     componentDidMount(){
         axios.get('https://localhost:44393/api/collection').then(res => {
             console.log(res);
@@ -13,6 +32,17 @@ export default class StackList extends React.Component {
     }
 
     render(){
-        return <ul>{this.state.stacks.map(stack => <li key={stack.id}>{stack.title}</li>)}</ul>;
+        return <form className="form-group" onSubmit={this.handleSubmit}>
+                <label>Choose a stack of cards:
+                  <select value={this.state.currentStack} onChange={this.handleChange}>
+                    {this.state.stacks.map(stack => <option key={`${stack.id}-${Date.now()}`} value={stack.title} >{stack.title}</option>)}
+                  </select>
+                </label>
+                <div className="form-group">
+                  <button className="btn-primary">Select Stack</button>
+                </div>
+          </form>;
+
+        // return <ul>{this.state.stacks.map(stack => <li key={stack.id}>{stack.title}</li>)}</ul>;
     }
 }
